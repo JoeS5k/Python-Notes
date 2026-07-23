ca2b3ec0e74a7ebcb99ce2390ce9beb66507edfb
JoeS5k <jstevens5k@gmail.com>
2026-07-23 13:19:55 +0000

    commit

---
+++ Lists.py
@@ -0,0 +1,9 @@
+#//////////////////// Lists ////////////////////
+My_List = []                                        #Creates an empty list | A list is used to store data 
+Fruits = ["Apple", "Banana", "Orange", "Kiwi"]      #Creates a list with data in it
+
+My_List.append("Item")                              #Adds an item to My_List
+My_List.remove("Item")                              #Removes an item from My_List
+print(Fruits[0])                                    #Prints the first item in Fruits | Index for any type of list starts at 0
+
+length = len(Fruits)                                #Gets the length of the List
---
+++ Variables.py
@@ -0,0 +1,24 @@
+#//////////////////// Variables ////////////////////
+
+x = 12            #This is an Integer(Int) | Stores numbers
+y = 10.0          #This is a Float(float)  | Stores numbers with decimals
+z = "Hello World" #This is a String(str)   | Stores words/phrases
+s = True          #This is a Boolean(bool) | Stores True/False values
+t = None          #This is a None Type     | Stores a value of nothing or none | not the same as 0 or "None"
+
+#//////////////////// operands ////////////////////
+
+ans = x + y       #Addition       -> 22.0
+ans = x - y       #Subtraction    -> 2.0
+ans = x * y       #Multiplication -> 120.0
+ans = x / y       #Division       -> 1.2
+ans = x // y      #Floor Division -> 1     | Rounds down the answer
+ans = x ** y      #Exponent       -> Big num
+ans = x % y       #Modulus        -> 2     | Gets the remainder
+
+#//////////////////// Print ///////////////////////
+
+print("Hello World")            #Output is | Hello World
+print(ans)                      #Output is | What is stored in ans ^^
+print(f"The answer is {ans}")   #Output is | The answer is 2.0(aka ans^^) 
+# The fString allows you to have a singular print function that can have a dyanmically changing varible in it
